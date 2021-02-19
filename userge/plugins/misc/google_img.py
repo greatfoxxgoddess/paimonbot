c5541eec12d0a49dfecb5189a7f4fd54a40268b9
Alícia <61088084+Thegreatfoxxgoddess@users.noreply.github.com>
2021-02-20 02:59:15 +0530

    Update google_img.py

--- userge/plugins/misc/google_img.py
+++ userge/plugins/misc/google_img.py
@@ -44,7 +44,7 @@ class Colors:
             "-d": "Upload as document",
             "-gif": "download gifs",
             "-down": "download only",
-            "colors": "any color in => ⚙️ Color",
+            "colors": "any color in => ✨ Color",
         },
         "usage": "{tr}gimg [flags] [query|reply to text]",
         "color": ["-" + _ for _ in Colors.choice],
@@ -71,7 +71,7 @@ async def gimg_down(message: Message):
     else:
         await message.err("`Input not found!...`", del_in=5)
         return
-    await message.edit("🔎")
+    await message.edit("`searching......`")
     start_t = datetime.now()
     color_ = None
     flags_ = message.flags
@@ -97,14 +97,14 @@ async def gimg_down(message: Message):
     else:
         arguments = await get_arguments(query=text)
     media_type = "Gifs" if allow_gif else "Pics"
-    await message.edit(f"⬇️  Downloading  {limit} {media_type} ...")
+    await message.edit(f"Downloading  {limit} {media_type} ...")
     try:
         results = await gimg_downloader(arguments)
     except Exception as e:
         await message.err(str(e), del_in=7)
         return
     if upload_:
-        await message.edit(f"⬆️  Uploading {limit} {media_type} ...")
+        await message.edit(f"Uploading {limit} {media_type} ...")
         try:
             await upload_image_grp(results, message, doc_)
         except Exception as err:
@@ -204,7 +204,7 @@ async def upload_image_grp(results, message: Message, doc: bool = False):
         for num, m_ in enumerate(mgroups, start=1):
             try:
                 await message.edit(
-                    f"⬆️  Uploading **{round(num / len(mgroups) * 100)} %** ..."
+                    f"Uploading **{round(num / len(mgroups) * 100)} %** ..."
                 )
                 await message.client.send_media_group(message.chat.id, media=m_)
                 await asyncio.sleep(len(m_))
